5132f1d04802434fad1ddd5b759ea79137ce0198
umipertiwi29 <umipertiwi26@gmail.com>
2020-10-16 19:51:02 +0700

    perpindahan_halaman

--- app/src/main/java/com/example/capstoneprojectk10/MainActivity.java
+++ app/src/main/java/com/example/capstoneprojectk10/MainActivity.java
@@ -24,23 +24,16 @@ public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
 
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
         setContentView(R.layout.activity_main);
-
-
-
-
-
     }
 
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, LihatSemuaActivity.class);
+        startActivity(intent);
+    }
 
 }
