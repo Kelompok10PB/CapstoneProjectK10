f716312ce65ef4d022ef556825456409d4b31f0c
wahyunb <wahyu31nb@gmail.com>
2020-11-10 01:31:41 +0700

    Add Some Features

---
+++ app/src/main/java/com/example/capstoneprojectk10/api/regulerData/RegulerDataHolder.java
@@ -0,0 +1,11 @@
+package com.example.capstoneprojectk10.api.regulerData;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface RegulerDataHolder {
+
+    @GET("public/api/update.json")
+    Call<RegulerData> getRegulerData();
+
+}
---
+++ app/src/main/java/com/example/capstoneprojectk10/vm/ProvDataViewModel.java
@@ -0,0 +1,35 @@
+package com.example.capstoneprojectk10.vm;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.capstoneprojectk10.api.provData.ProvData;
+import com.example.capstoneprojectk10.api.regulerData.RegulerData;
+import com.example.capstoneprojectk10.repo.ProvDataRepository;
+import com.example.capstoneprojectk10.repo.RegulerDataRepository;
+
+public class ProvDataViewModel extends ViewModel {
+    private MutableLiveData<ProvData> provData;
+    private MutableLiveData<Boolean> isLoading;
+    private ProvDataRepository provDataRepository;
+
+    public void init() {
+        if (provData != null){
+            return;
+        }
+        provDataRepository = ProvDataRepository.getInstance();
+        provData = provDataRepository.getProvData();
+        isLoading = provDataRepository.getLoading();
+
+    }
+
+    public LiveData<ProvData> getRegulerData() {
+        return provData;
+    }
+
+    public LiveData<Boolean> getLoading() {
+        return isLoading;
+    }
+
+}
