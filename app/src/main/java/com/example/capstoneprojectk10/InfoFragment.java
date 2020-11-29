e1604fcc58c272147e82a312b565af474b4f7704
wahyunb <wahyu31nb@gmail.com>
2020-11-29 23:40:25 +0700

    Add SQL Database and few things

--- app/src/main/java/com/example/capstoneprojectk10/InfoFragment.java
+++ app/src/main/java/com/example/capstoneprojectk10/InfoFragment.java
@@ -1,18 +1,22 @@
 package com.example.capstoneprojectk10;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class InfoFragment extends Fragment {
-
+    @BindView(R.id.nav_inf)
+    LinearLayout mSQL;
 
     @Nullable
     @Override
@@ -20,7 +24,15 @@ public class InfoFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_info, container, false);
         ButterKnife.bind(this, view);
 
+        mSQL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), CatatActivity.class);
+                getContext().startActivity(intent);
+            }
+        });
         return view;
     }
+
 
 }
